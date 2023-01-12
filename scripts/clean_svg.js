d6b74dabb27619fae162aee45ad7996d0d26f9b5
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-01-12 18:08:21 +0100

    Fixed add.js added config file for svgo. NOt 100% happy. Added first icons.

--- scripts/clean_svg.js
+++ scripts/clean_svg.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 const fs = require("fs");
 const { optimize } = require("svgo");
+const svgoConfig = require("./svgo.config.js");
 
 // Function 1: Optimizes the SVG file and returns the optimized content
 function optimizeSVG(file) {
@@ -13,118 +14,14 @@ function optimizeSVG(file) {
         if (fileExt !== "svg") {
           reject("File is not an SVG");
         }
-        const result = optimize(data.toString(), {
-          multipass: true,
-          eol: "lf",
-          plugins: [
-            "cleanupAttrs",
-            "inlineStyles",
-            "removeDoctype",
-            "removeXMLProcInst",
-            "removeComments",
-            "removeMetadata",
-            "removeDesc",
-            "removeUselessDefs",
-            "removeEditorsNSData",
-            "removeEmptyAttrs",
-            "removeHiddenElems",
-            "removeEmptyText",
-            "removeEmptyContainers",
-            "cleanupEnableBackground",
-            {
-              name: "convertPathData",
-              params: {
-                // 3 decimals of precision in floating point numbers
-                floatPrecision: 3,
-                // Some editors (e.g. Adobe Illustrator and Sketch) cannot parse flags
-                // without space wrapping
-                noSpaceAfterFlags: false,
-              },
-            },
-            "convertTransform",
-            {
-              name: "removeUnknownsAndDefaults",
-              params: {
-                // Keep the 'role' attribute, if it's already defined
-                keepRoleAttr: true,
-              },
-            },
-            "removeNonInheritableGroupAttrs",
-            {
-              // Remove paths with fill="none"
-              name: "removeUselessStrokeAndFill",
-              params: {
-                removeNone: true,
-              },
-            },
-            "removeUselessStrokeAndFill",
-            "removeUnusedNS",
-            "cleanupIds",
-            "cleanupNumericValues",
-            "cleanupListOfValues",
-            "moveGroupAttrsToElems",
-            "collapseGroups",
-            "removeRasterImages",
-            {
-              // Compound all <path>s into one
-              name: "mergePaths",
-              params: {
-                force: true,
-                noSpaceAfterFlags: false,
-              },
-            },
-            {
-              // Convert basic shapes (such as <circle>) to <path>
-              name: "convertShapeToPath",
-              params: {
-                // including <arc>
-                convertArcs: true,
-              },
-            },
-            "convertEllipseToCircle",
-            {
-              // Sort the attributes on the <svg> tag
-              name: "sortAttrs",
-              params: {
-                order: ["role", "viewBox"],
-                xmlnsOrder: "end",
-              },
-            },
-            "sortDefsChildren",
-            "removeDimensions",
-            {
-              name: "removeAttrs",
-              params: {
-                attrs: [
-                  "svg:(?!(role|viewBox|xmlns))",
-                  "path:(?!d)",
-                  "title:*",
-                ],
-              },
-            },
-            "removeElementsByAttr",
-            {
-              // Keep the role="img" attribute and automatically add it
-              // to the <svg> tag if it's not there already
-              name: "addAttributesToSVGElement",
-              params: {
-                attributes: [
-                  { role: "img", xmlns: "http://www.w3.org/2000/svg" },
-                ],
-              },
-            },
-            "removeOffCanvasPaths",
-            "removeStyleElement",
-            "removeScriptElement",
-            "reusePaths",
-          ],
-        });
+        const result = optimize(data.toString(), svgoConfig);
         resolve(result.data);
       }
     });
   });
 }
 
+//Creates a 256 hash of a string (in this case an optimized svg)
 function sha256Hash(data) {
   const hash = crypto.createHash("sha256");
   hash.update(data);
